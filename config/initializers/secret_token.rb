e4a4e85b7a35b21152eaae0517b8687fb4699212
valeriemettler <valeriemettler@gmail.com>
2014-08-20 18:21:41 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocipedia::Application.config.secret_key_base = '54aca17ce78b2753a0e8e7d97c9c1c10d87aeac8ba135149a87e6100b52058063b6705616d3a6a417c37676d953d1179a9858f2b53bc020966284a0bb117f8d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blocipedia::Application.config.session_store :cookie_store, key: '_Blocipedia_session'
