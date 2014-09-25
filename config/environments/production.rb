0fa5d120c5465b7d39b688a374485fb26e4b39d1
Valerie Mettler <valeriemettler@gmail.com>
2014-09-24 19:18:30 -0500

    this is a test to fix user registration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,7 @@ Blocipedia::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  Rails.application.routes.default_url_options[:host] = '???'
+
   config.action_mailer.default_url_options = { host: 'valblocipedia.herokuapp.com' }
 end
